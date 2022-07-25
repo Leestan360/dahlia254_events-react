4016493fccfca7255c4876239b976db79b92ea44
Leestan360 <stanleymayore2@gmail.com>
2022-07-25 16:01:13 +0300

    Add components for the site

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h2>Dahlia254_Events</h2>
+      <div>
+        <p>About</p>
+        <p>Events</p>
+        <p>Weddings</p>
+        <button>Login</button>
+        <button>Signup</button>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;
