184847c361b8ffe2ba4fff21e5e5efede5e47ba5
Leestan360 <stanleymayore2@gmail.com>
2022-07-26 10:02:22 +0300

    Add user to database

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -3,16 +3,35 @@ import React, { useEffect, useState } from "react";
 
 function Authentication() {
 
-  const [users, setUsers] = useState({});
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
 
-  useEffect(() => {
-    fetch("https://dahlia254-react.herokuapp.com/users")
-    .then(res => res.json())
-    .then(user => {
-      setUsers(user)
-      console.log(user);
+  function handleChange(e) {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("https://dahlia254-react.herokuapp.com/users", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({firstName: formData.firstName, lastName: formData.lastName, email: formData.email,
+      password: formData.password, confirmPassword: formData.confirmPassword})
     })
-  }, []);
+    .the(response => response.json())
+    .the(data => {
+      onAddUser(data)
+      setFormData({...formData, firstName: "", lastName: "", email: "", password: "", confirmPassword: ""})
+    })
+  }
+
 
   return (
     <div></div>
