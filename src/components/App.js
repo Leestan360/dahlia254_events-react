ba6ef0b7824450ef20b9f4bc9a5a439c6b93dbee
Leestan360 <stanleymayore2@gmail.com>
2022-07-26 19:40:11 +0300

    Add routes to all other components

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Authentication from "./Authentication";
 import NavBar from "./NavBar";
-// import EventsList from "./EventsPage";
-// import { Route, Routes } from "react-router-dom";
-// import WeddingsList from "./WeddingsPage";
-// import Login from "./Login";
+import Login from "./Login";
+import EventsList from "./EventsList";
+import WeddingsList from "./WeddingsList";
 
 function App() {
 
@@ -14,7 +13,12 @@ function App() {
   <div>
     <NavBar />
     <Routes>
-      <Route exact path="/signup" element={<Authentication />} /> 
+      <Route exact path="/signup" element={<Authentication />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/about" />
+      <Route exact path="/events" element={<EventsList />} />  
+      <Route exact path="/weddings" element={<WeddingsList />} />  
+      <Route exact path="/" element={<Authentication />} />  
     </Routes>
   </div>
   );
